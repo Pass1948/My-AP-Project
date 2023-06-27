5e11459082d35e3d3a61ab3216198e26645fb82a
pass1948 <dldudtls100@gmail.com>
2023-06-27 17:52:28 +0900

    feat : 달리기 버튼 및 애니메이션 구현

--- Assets/Script/Player/AdventurePlayer/AdventurePlayer.cs
+++ Assets/Script/Player/AdventurePlayer/AdventurePlayer.cs
@@ -14,73 +14,24 @@ public class AdventurePlayer : MonoBehaviour
     private CharacterController controller;
     private Animator animator;
     private Vector3 moveDir;
-    private float moveSpeed;
     private float ySpeed = 0;
     private float curSpeed;
-    private bool isWalking;
+    private bool isRun = false;
 
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
     }
-
-    private void OnEnable()
-    {
-        moveRoutine = StartCoroutine(MoveRoutine());
-        jumpRoutine = StartCoroutine(JumpRoutine());
-    }
-
-    private void OnDisable()
-    {
-        StopCoroutine(moveRoutine);
-        StopCoroutine(jumpRoutine);
-    }
-
     private void Update()
     {
-       
+        Fall();
+        Move();
     }
 
     private void FixedUpdate()
     {
-        //Move();
-        //GroundCheck();
-    }
-
-    Coroutine moveRoutine;
-    private IEnumerator MoveRoutine()
-    {
-        while (true)
-        {
-            if (moveDir.sqrMagnitude <= 0)
-            {
-                curSpeed = Mathf.Lerp(curSpeed, 0, 0.1f);
-                //animator.SetFloat("MoveSpeed", curSpeed);
-                yield return null;
-                continue;
-            }
-
-            Vector3 fowardVec = new Vector3(Camera.main.transform.forward.x, 0f, Camera.main.transform.forward.z).normalized;
-            Vector3 rightVec = new Vector3(Camera.main.transform.right.x, 0f, Camera.main.transform.right.z).normalized;
-
-            if (isWalking)
-            {
-                curSpeed = Mathf.Lerp(curSpeed, walkSpeed, 0.1f);
-            }
-            else
-            {
-                curSpeed = Mathf.Lerp(curSpeed, runSpeed, 0.1f);
-            }
-
-            controller.Move(fowardVec * moveDir.z * curSpeed * Time.deltaTime);
-            controller.Move(rightVec * moveDir.x * curSpeed * Time.deltaTime);
-            //animator.SetFloat("MoveSpeed", curSpeed);
-
-            Quaternion lookRotation = Quaternion.LookRotation(fowardVec * moveDir.z + rightVec * moveDir.x);
-            transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, 0.2f);
-
-        }
+        GroundCheck();
     }
 
     void Move()
@@ -88,20 +39,22 @@ public class AdventurePlayer : MonoBehaviour
         // Mathf.Lerp() 애니메이션에 부드러운 전환을 넣기위한 정밀작업
         if (moveDir.magnitude == 0)
         {
-            curSpeed = Mathf.Lerp(curSpeed, 0, 0.1f);
-            //animator.SetFloat("MoveSpeed", curSpeed);
+            curSpeed = Mathf.Lerp(curSpeed, 0, 0.5f);
+            animator.SetFloat("MoveSpeed", curSpeed);
+            isRun = false;
             return;
         }
-
-        else if (isWalking)             // 움직일 경우
+        else if (isRun)
         {
-            moveSpeed = Mathf.Lerp(moveSpeed, walkSpeed, 0.5f);
+            curSpeed = runSpeed;
+            animator.SetBool("IsRun", true);
+        }
+        else
+        {
+            curSpeed = walkSpeed;
+            animator.SetBool("IsRun", false);
         }
 
-        else                            // 뛸경우
-        {
-            moveSpeed = Mathf.Lerp(moveSpeed, runSpeed, 0.5f);
-        }
 
         Vector3 vecFor = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z).normalized;
         Vector3 vecRig = new Vector3(Camera.main.transform.right.x, 0, Camera.main.transform.right.z).normalized;
@@ -112,12 +65,11 @@ public class AdventurePlayer : MonoBehaviour
 
         Quaternion lookRotation = Quaternion.LookRotation(vecFor * moveDir.z + vecRig * moveDir.x);
         transform.rotation = Quaternion.Lerp(lookRotation, transform.rotation, 0.1f);
+    }
 
-
-        //animator.SetFloat("XSpeed", moveDir.x, 0.1f, Time.deltaTime);
-        //animator.SetFloat("YSpeed", moveDir.z, 0.1f, Time.deltaTime);
-
-        //animator.SetFloat("Speed", moveSpeed);
+    private void OnRun(InputValue value)
+    {
+            isRun = value.isPressed;
     }
 
     void OnMove(InputValue value)
@@ -125,35 +77,28 @@ public class AdventurePlayer : MonoBehaviour
         moveDir.x = value.Get<Vector2>().x;
         moveDir.z = value.Get<Vector2>().y;
     }
-    private void OnWalk(InputValue value)
-    {
-        isWalking = value.isPressed;
-    }
 
-    Coroutine jumpRoutine;
-    private IEnumerator JumpRoutine()
+    private void Fall()
     {
-        while (true)
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        if (controller.isGrounded && ySpeed < 0)
         {
-            ySpeed += Physics.gravity.y * Time.deltaTime;
-
-            if (GroundCheck() && ySpeed < 0)
-                ySpeed = -1;
-
-            controller.Move(Vector3.up * ySpeed * Time.deltaTime);
-            yield return null;
+            ySpeed = 0;
         }
-    }
-    void Jump()
-    {
-        //rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+
+        controller.Move(Vector3.up * ySpeed * Time.deltaTime);
     }
 
-    void OnJump(InputValue value)
+    private void Jump()
     {
+        ySpeed = jumpPower;
+    }
 
+    private void OnJump(InputValue value)
+    {
         if (GroundCheck())
-            ySpeed = jumpPower; 
+        Jump();
     }
 
     private bool GroundCheck()
